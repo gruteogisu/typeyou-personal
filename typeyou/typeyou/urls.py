2002b5036d8f920ca5fe6d4037ac45eab51ad540
gruteogisu <gruteogisu@gmail.com>
2016-06-25 02:05:54 +0900

    refactor urls: devide urls into each app

--- typeyou/typeyou/urls.py
+++ typeyou/typeyou/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 
 from typeyou.views import *
@@ -7,4 +7,8 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
 
     url(r'^$', HomeView.as_view(), name="home"),
+
+    url(r'^', include("users.urls", namespace="users")),
+
+    url(r'^community/', include("community.urls", namespace="community")),
 ]
